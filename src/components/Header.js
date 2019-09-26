35068e65a8d0fb1790f137f0c89fe33dc20acff1
DerekSgov <50340998+DerekSgov@users.noreply.github.com>
2019-09-26 11:03:16 -0700

    First commit, Adding files

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import logo from '../assets/images/logo.svg';
+
+const Header = (props) => (
+    <header id="header" className="alt">
+        <span className="logo"><img src={logo} alt="" /></span>
+        <h1>Snowlutions</h1>
+        <p>Predictive Avalanche Solutions</p>
+    </header>
+)
+
+export default Header
