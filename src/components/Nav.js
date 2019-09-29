c3488be0fcc859f274895c42045f8e937be73962
DerekSGov <Derekgsiemens@gmail.com>
2019-09-29 14:56:43 -0700

    Adding our content, headshots and etc, really just customizing the template

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,22 +7,17 @@ const Nav = (props) => (
         <Scrollspy items={ ['intro', 'first', 'second', 'cta'] } currentClassName="is-active" offset={-300}>
             <li>
                 <Scroll type="id" element="intro">
-                    <a href="#">Introduction</a>
+                    <a href="#">Our Mission</a>
                 </Scroll>
             </li>
             <li>
                 <Scroll type="id" element="first">
-                    <a href="#">First Section</a>
-                </Scroll>
-            </li>
-            <li>
-                <Scroll type="id" element="second">
-                    <a href="#">Second Section</a>
+                    <a href="#">Our Team</a>
                 </Scroll>
             </li>
             <li>
                 <Scroll type="id" element="cta">
-                    <a href="#">Get Started</a>
+                    <a href="#">Requirements document</a>
                 </Scroll>
             </li>
         </Scrollspy>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,13 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import { Waypoint } from 'react-waypoint'
 import pic01 from '../assets/images/pic01.jpg'
+import Jerusha from '../assets/images/Jerusha.jpg'
+import Andrew from '../assets/images/Andrew.jpg'
+import Samuel from '../assets/images/Samuel.jpg'
+import Alex from '../assets/images/Alex.jpg'
+import Derek from '../assets/images/Derek.jpg'
+import ShoYa from '../assets/images/Sho.jpg'
+import Charles from '../assets/images/Charles.jpg'
 import Header from '../components/Header'
 import Layout from '../components/layout'
 import Nav from '../components/Nav'
@@ -41,142 +48,67 @@ class Index extends React.Component {
             <div className="spotlight">
               <div className="content">
                 <header className="major">
-                  <h2>Ipsum sed adipiscing</h2>
+                  <h2>Our Mission</h2>
                 </header>
-                <p>
-                  Sed lorem ipsum dolor sit amet nullam consequat feugiat
-                  consequat magna adipiscing magna etiam amet veroeros. Lorem
-                  ipsum dolor tempus sit cursus. Tempus nisl et nullam lorem
-                  ipsum dolor sit amet aliquam.
-                </p>
-                <ul className="actions">
-                  <li>
-                    <Link to="/generic" className="button">
-                      Learn More
-                    </Link>
-                  </li>
-                </ul>
+                <p>Our mission at Snowlutions is to provide cutting edge modern solutions to an age old problem. We employ advanced predictive models and an array of data sources to find out when and where avalanches will happen before they do, reducing the risk for outdoor enthusiasts to do what they love.</p>
               </div>
               <span className="image">
-                <img src={pic01} alt="" />
+                <img src={'https://images.unsplash.com/photo-1555104876-061df4ef2c45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80'} alt="" />
               </span>
             </div>
           </section>
-
           <section id="first" className="main special">
             <header className="major">
-              <h2>Magna veroeros</h2>
+              <h2>Our Team</h2>
             </header>
             <ul className="features">
               <li>
-                <span className="icon major style1 fa-code"></span>
-                <h3>Ipsum consequat</h3>
-                <p>
-                  Sed lorem amet ipsum dolor et amet nullam consequat a feugiat
-                  consequat tempus veroeros sed consequat.
-                </p>
+                <span className="image headshot"><img src={Jerusha} alt=""/></span>
+                <h3>Chua, Jerusha</h3>
+                <p>Position Here</p>
               </li>
               <li>
-                <span className="icon major style3 fa-copy"></span>
-                <h3>Amed sed feugiat</h3>
-                <p>
-                  Sed lorem amet ipsum dolor et amet nullam consequat a feugiat
-                  consequat tempus veroeros sed consequat.
-                </p>
+              <span className="image headshot"><img src={Andrew} alt=""/></span>
+                <h3>Eng, Andrew</h3>
+                <p>Position Here</p>
               </li>
               <li>
-                <span className="icon major style5 fa-diamond"></span>
-                <h3>Dolor nullam</h3>
-                <p>
-                  Sed lorem amet ipsum dolor et amet nullam consequat a feugiat
-                  consequat tempus veroeros sed consequat.
-                </p>
+              <span className="image headshot"><img src={Samuel} alt=""/></span>
+                <h3>Joy, Samuel</h3>
+                <p>Position Here</p>
+              </li>
+              <li>
+              <span className="image headshot"><img src={Alex} alt=""/></span>
+                <h3>Schell, Alex</h3>
+                <p>Position Here</p>
+              </li>
+              <li>
+              <span className="image headshot"><img src={Derek} alt=""/></span>
+                <h3>Siemens, Derek</h3>
+                <p>Position Here</p>
+              </li>
+              <li>
+              <span className="image headshot"><img src={ShoYa} alt=""/></span>
+                <h3>Voorthuyzen, Sho Ya</h3>
+                <p>Position Here</p>
+              </li>
+              <li>
+              <span className="image headshot"><img src={Charles} alt=""/></span>
+                <h3>Yang, Charles</h3>
+                <p>Position Here</p>
               </li>
             </ul>
-            <footer className="major">
-              <ul className="actions">
-                <li>
-                  <Link to="/generic" className="button">
-                    Learn More
-                  </Link>
-                </li>
-              </ul>
-            </footer>
           </section>
-
-          <section id="second" className="main special">
-            <header className="major">
-              <h2>Ipsum consequat</h2>
-              <p>
-                Donec imperdiet consequat consequat. Suspendisse feugiat congue
-                <br />
-                posuere. Nulla massa urna, fermentum eget quam aliquet.
-              </p>
-            </header>
-            <ul className="statistics">
-              <li className="style1">
-                <span className="icon fa-code-fork"></span>
-                <strong>5,120</strong> Etiam
-              </li>
-              <li className="style2">
-                <span className="icon fa-folder-open-o"></span>
-                <strong>8,192</strong> Magna
-              </li>
-              <li className="style3">
-                <span className="icon fa-signal"></span>
-                <strong>2,048</strong> Tempus
-              </li>
-              <li className="style4">
-                <span className="icon fa-laptop"></span>
-                <strong>4,096</strong> Aliquam
-              </li>
-              <li className="style5">
-                <span className="icon fa-diamond"></span>
-                <strong>1,024</strong> Nullam
-              </li>
-            </ul>
-            <p className="content">
-              Nam elementum nisl et mi a commodo porttitor. Morbi sit amet nisl
-              eu arcu faucibus hendrerit vel a risus. Nam a orci mi, elementum
-              ac arcu sit amet, fermentum pellentesque et purus. Integer maximus
-              varius lorem, sed convallis diam accumsan sed. Etiam porttitor
-              placerat sapien, sed eleifend a enim pulvinar faucibus semper quis
-              ut arcu. Ut non nisl a mollis est efficitur vestibulum. Integer
-              eget purus nec nulla mattis et accumsan ut magna libero. Morbi
-              auctor iaculis porttitor. Sed ut magna ac risus et hendrerit
-              scelerisque. Praesent eleifend lacus in lectus aliquam porta. Cras
-              eu ornare dui curabitur lacinia.
-            </p>
-            <footer className="major">
-              <ul className="actions">
-                <li>
-                  <Link to="/generic" className="button">
-                    Learn More
-                  </Link>
-                </li>
-              </ul>
-            </footer>
-          </section>
-
           <section id="cta" className="main special">
             <header className="major">
-              <h2>Congue imperdiet</h2>
-              <p>
-                Donec imperdiet consequat consequat. Suspendisse feugiat congue
-                <br />
-                posuere. Nulla massa urna, fermentum eget quam aliquet.
-              </p>
+              <h2>Requirements document</h2>
+              <p>As a response to the RFP put forward by Steep Mountaineering, we have put together this requirments document for a solution system.</p>
             </header>
             <footer className="major">
               <ul className="actions">
                 <li>
                   <Link to="/generic" className="button special">
-                    Get Started
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/generic" className="button">
-                    Learn More
+                    View Document
                   </Link>
                 </li>
               </ul>
